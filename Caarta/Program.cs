193121ede68f531ea5aa2987730e9329cdf28ff8
Palatacemy <g.s.tsonev@gmail.com>
2025-02-20 20:00:14 +0200

    Seeders

--- Caarta/Program.cs
+++ Caarta/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Caarta.Services.Abstractions;
 using Caarta.Services.Services;
 using Microsoft.AspNetCore.Identity;
+using CatsMVC.Data.Seeders;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,7 +18,9 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseLazyLoadingProxies();
 });
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddDefaultIdentity<AppUser>(options => {
     options.SignIn.RequireConfirmedAccount = false;
 }).AddEntityFrameworkStores<ApplicationDbContext>();
@@ -36,7 +39,11 @@ builder.Services.AddScoped<ILanguageService, LanguageService>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
-
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    UserSeeder.Initialize(services).Wait();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
